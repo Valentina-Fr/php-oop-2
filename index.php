ba5fa676b1b402339f467150466db5c242e0b2e9
Valentina-Fr <v_frasca@virgilio.it>
2021-10-06 16:45:11 +0200

    Add customer and premium classes

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
 require_once __DIR__.'/models/product.php';
+require_once __DIR__.'/models/user.php';
+
 
 $product1 = new Product("phone", "Apple", 1000);
-
 var_dump($product1);
+
+$user1 = new User("Mario", "Rossi", "mr@gmail.com");
+$premium = new Premium("Jane", "Doe", "jd@hotmail.com", "3");
+
+var_dump($user1);
+var_dump($premium);
---
+++ models/user.php
@@ -0,0 +1,22 @@
+<?php
+
+class User {
+    protected $name;
+    protected $last_name;
+    protected $email;
+
+    public function __construct($_name, $_last_name, $_email) {
+        $this->name = $_name;
+        $this->last_name = $_last_name;
+        $this->email = $_email;
+    }
+}
+
+class Premium extends User {
+    protected $level;
+    
+    public function __construct($_name, $_last_name, $_email, $_level = 0) {
+        parent:: __construct ($_name, $_last_name, $_email);
+        $this->level = $_level;
+    }
+}
